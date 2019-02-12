f032927d660b89c6fcc04cb3d108134d427b2c54
simk395 <simon.kong95@gmail.com>
2019-02-12 11:50:33 -0500

    summoner bg

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   resources :summoners
-  resources :games, only :show
-  resources :match_histories, only :show
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :summoners
   resources :games
   resources :match_histories
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
