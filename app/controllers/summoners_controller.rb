e7866155f13a8dbfde5c08fe365cac53e15e70d0
simk395 <simon.kong95@gmail.com>
2019-02-12 10:30:57 -0500

    on
    Signed-off-by: simk395 <simon.kong95@gmail.com>

--- app/controllers/summoners_controller.rb
+++ app/controllers/summoners_controller.rb
@@ -3,11 +3,24 @@ class SummonersController < ApplicationController
         #login
     end
 
+    def create
+        @summoner = Summoner.new(summoner_params)
+        if summoner.save
+            redirect_to summoner_path
+        else
+            redirect_to new_summoner
+        end
+    end
+
     def new
         @summoner = Summoner.new
     end
 
     def show
         
+    end
+
+    def summoner_params
+        params.require(:summoner).require(:summoner_name, :password, :password_confirmation)
     end
 end
