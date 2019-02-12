fe5260d3a04c8cef271a456e98cbf68e30505e35
John Oncher <johnoncher@gmail.com>
2019-02-12 09:03:59 -0500

    images and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :summoners
+  resources :games, only :show
+  resources :match_histories, only :show
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
