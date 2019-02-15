f33cdb1624d7bba8db268526265f83ede12a662a
John Oncher <johnoncher@gmail.com>
2019-02-15 09:28:50 -0500

    working copy

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
   resources :summoners
   resources :games, only: [:show]
   resources :match_histories
-  post "/games", to: "games#create", as: 'games' 
+  post "/games", to: "games#create", as: 'games'
   get "/", to: "summoners#new", as: "homepage"
   delete "/summoners", to: "summoners#destroy", as: "log_out"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
 end
