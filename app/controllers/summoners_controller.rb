7a136212541b407b041c2647de1246d1ebfef78f
John Oncher <johnoncher@gmail.com>
2019-02-15 11:03:57 -0500

    changes

--- app/controllers/summoners_controller.rb
+++ app/controllers/summoners_controller.rb
@@ -4,13 +4,13 @@ class SummonersController < ApplicationController
     end
 
     def create
-      
+
         @summoner = Summoner.find_or_initialize_by(summoner_name: summoner_params)
         profile = @summoner.league_profile
         if profile != 404
             @summoner.acc_id = profile["accountId"]
             @summoner.save
-            session[:summoner_id] = @summoner.id      
+            session[:summoner_id] = @summoner.id
             # redirect_to summoner_path(session[:summoner_id])
             redirect_to summoner_path(session[:summoner_id])
         else
@@ -33,9 +33,11 @@ class SummonersController < ApplicationController
     def update
       @summoner = Summoner.find(params[:id])
       profile = @summoner.league_profile
+      @summoner.acc_id = profile["accountId"]
       if profile != 404
           @summoner.update(summoner_name: summoner_params)
           session[:summoner_id] = Summoner.find(params[:id])
+          session[:acc_id] = profile["accountId"]
 
           redirect_to summoner_path(session[:summoner_id])
       else
@@ -49,6 +51,7 @@ class SummonersController < ApplicationController
       @account = @summoner.league_profile
       @profile = @summoner.acc_id
       @matches = @summoner.recent_10_games
+      byebug
       @game = Game.new
     end
 
